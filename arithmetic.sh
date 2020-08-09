c1e5d27b02c5f24f231c189092966b7b40b60dd9
mrun255dumbre <mrun255dumbre@gmail.com>
2020-08-09 08:29:25 +0530

    sixth branch

--- arithmetic.sh
+++ arithmetic.sh
@@ -9,3 +9,8 @@ compute_1=$(( $a + $b * $c ))
 compute_2=$(( $a * $b + $c ))
 compute_3=$(( $c + $a / $b ))
 compute_4=$(( $a % $b + $c ))
+
+dict[compute1]=$compute_1
+dict[compute2]=$compute_2
+dict[compute3]=$compute_3
+dict[compute4]=$compute_4
